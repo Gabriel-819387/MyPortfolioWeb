ef54816f13b771fff4e26cf0c73a1724171f9a62
Gabriel-819387 <71510540+Gabriel-819387@users.noreply.github.com>
2020-11-05 15:21:42 -0500

    Addition
    Added Buttons and background Image

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,3 +1,14 @@
 function sayHello() {
-    console.log('gabe was here')
+    console.log('gabe was here');
+}
+
+function saybye() {
+    let name= prompt("What is your name?");
+    
+    if (name=="Gabriel") {
+        alert("Hey my name is Gabriel too!");
+    }
+    else{
+        alert("Cool Name! +");
+    }
 }
